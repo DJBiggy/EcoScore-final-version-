6570fdfbb0ba7f1fcbefcd1810b323180eda27df
Juanathan Bean <randot771@gmail.com>
2024-01-31 08:33:38 -0500

    Initial Commit

---
+++ EcoScore (final version)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EcoScore (final version)
+//
+//  Created by 1a on 1/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
